a7df788565f6f9e5dc161a0d6abafd23b2f93806
Juan José Sarasola <58186406+jjsarasola@users.noreply.github.com>
2023-08-16 22:24:38 +0200

    Update ubuntu_zabbix_agent.sh

--- ubuntu_zabbix_agent.sh
+++ ubuntu_zabbix_agent.sh
@@ -3,6 +3,9 @@
 # Prompt user for the Zabbix server IP address
 read -p "Enter the Zabbix server IP address: " ZABBIX_SERVER_IP
 
+# Detect the system hostname - Uncomment the following line if you want to automatically set the Zabbix hostname parameter to the value of the server hostname.
+# HOSTNAME=$(hostname)
+
 # Update the package list and install the Zabbix agent
 sudo apt update
 sudo apt install zabbix-agent -y
@@ -10,6 +13,9 @@ sudo apt install zabbix-agent -y
 # Update the Zabbix agent configuration file
 sudo sed -i "s/Server=127.0.0.1/Server=$ZABBIX_SERVER_IP/g" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i "s/ServerActive=127.0.0.1/ServerActive=$ZABBIX_SERVER_IP/g" /etc/zabbix/zabbix_agentd.conf
+
+# Uncomment the following line if you want to automatically set the Zabbix hostname parameter to the value of the server hostname.
+# sudo sed -i "s/# Hostname=/Hostname=$HOSTNAME/g" /etc/zabbix/zabbix_agentd.conf
 
 # Start and enable the Zabbix agent service
 sudo systemctl start zabbix-agent
